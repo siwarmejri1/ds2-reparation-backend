4a643e1e2b451106baa23b5662154975268afab2
Mejri Siwar <mejrisiwar473@gmail.com>
2025-12-12 09:45:14 +0100

    feat(parts): create SparePart entity
    - Add fields: name, price, Stock
    - Add timestamps for tracking

---
+++ src/parts/entities/spare-part.entity.ts
@@ -0,0 +1,27 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, ManyToMany } from 'typeorm';
+// importina l entité mtaa l intervention besh naamlou l relation mtaa spare parts
+import { Intervention } from '../../interventions/entities/intervention.entity';
+// sna3na l entité mtaa spare part
+@Entity('spare_parts')
+export class SparePart {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({ default: 0 })
+  stock: number;
+
+  @Column('decimal', { precision: 10, scale: 2 })
+  price: number;
+//besh naamlou relation mtaa spare part m3a intervention
+  @ManyToMany(() => Intervention, (intervention) => intervention.spareParts)
+  interventions: Intervention[];
+// 3malna timestamps besh na3rfou waqt l creation w l update mtaa l spare part
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
