ddda6d1c216887d5671bbe10ae27a98840fbd6c8
Fatmaderouiche <derouicheftm@gmail.com>
2025-12-17 13:28:00 +0100

    feat(users): create admin using RegisterDto with guards
    -add create-admin method  in UsersService using RegisterDto
    - add role to use ADMIN
    - Protect route with JwtAuthGuard and IsAdminGuard on all the controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { JwtAuthGuard } from '../auth/guards/jwt-auth/jwt-auth.guard';
-import { IsAdminGuard } from '../auth/guards/is-admin/is-admin.guard';
+import { Controller, Get, Req, UseGuards, Post, Body} from '@nestjs/common';
+import { JwtAuthGuard } from '../../src/common/guards/jwt-auth/jwt-auth.guard';
+import { IsAdminGuard } from '../../src/common/guards/is-admin/is-admin.guard';
 import { UsersService } from './user.service';
+import { RegisterDto } from '../auth/dto/register.dto';
 
+@UseGuards(JwtAuthGuard, IsAdminGuard) // houni 7atina les guards 3al controller kolou
 @Controller('users')
 export class UsersController {
     constructor(private  usersService: UsersService) {}
@@ -10,8 +12,13 @@ export class UsersController {
 
 // fonction nraj3ou beha profile mte3 user kenou authentifié w admin bel gaurds
 @Get('profile')
-  @UseGuards(JwtAuthGuard, IsAdminGuard)
   getProfile(@Req() req) {
     return this.usersService.getProfile(req.user.userId);
+  }
+
+// route bch nasn3ou admin jdid (suelemnt les admins ynejmou yest3amlouha )
+@Post('create-admin')
+  createAdmin(@Body() registerDto: RegisterDto) {
+    return this.usersService.createAdmin(registerDto);
   }
 }
