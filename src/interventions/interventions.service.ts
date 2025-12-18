09989f9bb0fb0401d92ad947fe89dfffbab8afa3
Fatmaderouiche <derouicheftm@gmail.com>
2025-12-18 14:18:50 +0100

    fix(intervention): correct user assignment in controller - Replace incorrect 'technician' with 'user' - Pass JWT user correctly to Interventions Service

--- src/interventions/interventions.service.ts
+++ src/interventions/interventions.service.ts
@@ -22,10 +22,10 @@ export class InterventionService { // service responsable 3al logique mta3 inter
     private readonly interventionRepository: Repository<Intervention>, // repository mta3 interventions
 
     @InjectRepository(Device)
-    private readonly devicesRepository: Repository<Device>, // repository mta3 devices
+    private readonly deviceRepository: Repository<Device>, // repository mta3 devices
 
     @InjectRepository(SparePart)
-    private readonly sparePartsRepository: Repository<SparePart>, // repository mta3 spare parts
+    private readonly sparePartRepository: Repository<SparePart>, // repository mta3 spare parts
 
     private readonly dataSource: DataSource, // DataSource mta3 TypeORM lezmou 3al transaction
     private readonly devicesService: DevicesService,
@@ -35,7 +35,7 @@ export class InterventionService { // service responsable 3al logique mta3 inter
   async create(createInterventionDto: CreateInterventionDto, user: User)  // logique mta3 création intervention
    : Promise<Intervention> {
       // hnee besh naamlou transaction 3al database barcha opérations lezmhom ykounou atomic
-      const device = await this.devicesRepository.findOne({
+      const device = await this.deviceRepository.findOne({
       where: { id: createInterventionDto.deviceId },
       });
 
@@ -44,7 +44,7 @@ export class InterventionService { // service responsable 3al logique mta3 inter
       }
 
       // hedhi besh nverifiou byha ken spare parts valides w mawjoudin kolhom
-    const spareParts = await this.sparePartsRepository.findByIds(
+    const spareParts = await this.sparePartRepository.findByIds(
       createInterventionDto.sparePartIds,
     );
       if (spareParts.length !== createInterventionDto.sparePartIds.length) {
@@ -71,11 +71,14 @@ export class InterventionService { // service responsable 3al logique mta3 inter
       for (const sparePart of spareParts) {
         await this.partsService.decrementStock(sparePart.id, 1);
       }
-    
+    //nrecuperiw l user technicien mel jwt w nekhdou menno id w email bark
+      const tech = await this.dataSource.manager.findOne(User, { where: { id: user.id }
+        ,select: ['id', 'email'], });
+        
       // besh nassn3ou l intervention
       const intervention = queryRunner.manager.create(Intervention, {
         description: createInterventionDto.description,
-        technician: user, // injectineha mel jwt
+        user: tech, // injectineha mel jwt
         device : device,
         spareParts: spareParts,
       });
@@ -103,13 +106,13 @@ export class InterventionService { // service responsable 3al logique mta3 inter
 
   async findAll(): Promise<Intervention[]> { // logique mta3 recuperation lkol interventions
     return this.interventionRepository.find({
-      relations: ['device', 'technician', 'spareParts'],// njibou m3ahom les relations mte3hom
+      relations: ['device', 'user', 'spareParts'],// njibou m3ahom les relations mte3hom
     });
   }
     async findOne(id: number): Promise<Intervention> { // logique mta3 recuperation intervention b id mte3ou
     const intervention = await this.interventionRepository.findOne({
       where: { id },
-      relations: ['device', 'technician', 'spareParts'],
+      relations: ['device', 'user', 'spareParts'],
     });
     
     if (!intervention) { //ken ma l9ach intervention b id hedha
